904322995e10e3071b84c7d96379cd25aac49b04
nicholasfiore <89951180+nicholasfiore@users.noreply.github.com>
2023-10-02 18:09:26 -0400

    More work with app.js
    Testing the use of EJS

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('view engine', 'html');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
-app.get('/', (req, res) => {
-    res.render('landing');
-});
+// app.get('/', (req, res) => {
+//     res.('homepage.html');
+// });
 
 app.listen(3000, () => {
     console.log('App Listening to port 3000');
