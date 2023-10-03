feed722211a84766353cdb7a42c71d6533ea0a28
nicholasfiore <89951180+nicholasfiore@users.noreply.github.com>
2023-10-03 15:57:29 -0400

    Webpages now properly render with Express, using EJS and CSS

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
-// app.get('/', (req, res) => {
-//     res.('homepage.html');
-// });
+app.get('/', (req, res) => {
+    res.render('profile_view');
+});
 
 app.listen(3000, () => {
     console.log('App Listening to port 3000');
