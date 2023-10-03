4a15d6ac644df7625466df35dea9ca7e73917740
nicholasfiore <89951180+nicholasfiore@users.noreply.github.com>
2023-10-03 16:40:37 -0400

    Adjusted initial render pages, working on routing

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('profile_view');
+    res.render('landing');
+});
+
+app.get('/home', (req, res) => {
+    res.render('homepage');
 });
 
 app.listen(3000, () => {
