117c4eb32348c413efdc06a72d87ffbb3d1279af
Hanna Sharkey <hthesharkey@gmail.com>
2023-11-04 20:40:28 -0400

    database.js updated to connect to mysql and send queries

---
+++ database.js
@@ -0,0 +1,28 @@
+const mysql = require('mysql2');
+//const mysql = require('mysql');
+const connection = mysql.createConnection({
+    //host: "localhost:3000",
+    host: "localhost",
+    user: "root",
+    password: "example",
+    database: "FacultyElectionsDB",
+});
+
+function query(queryStr, callback) {
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            console.log(err);
+            return callback(err);
+        }
+        connection.query(queryStr, function(err, results) {
+            connection.release(); // always put connection back in pool after last query
+            if (err){
+                    console.log(err);
+                    return callback(err);
+                }
+            callback(null, results);
+        });
+    });
+}
+
+module.exports.query = query;
