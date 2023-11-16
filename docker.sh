de9b004d3d6cd477d213a7511ffac031c7ffe491
nicholas.fiore1@marist.edu <nicholas.fiore1@marist.edu>
2023-11-15 20:10:32 -0500

    Adding CAS authentication
    Added cas. Also created a dev "test" user for offline testing

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker compose create --build
+docker compose create --build --remove-orphans
 
 docker tag marist-faculty-elections-server nicfio25/marist-faculty-elections-server:dev
 
