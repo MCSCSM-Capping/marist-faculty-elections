25c0cef715f3b4637f3b52ffb699b75c4bfbf7b7
Hanna Sharkey <hthesharkey@gmail.com>
2023-11-29 16:57:22 -0500

    fix test user database insert

--- misc/facultytables.sql
+++ misc/facultytables.sql
@@ -19,7 +19,7 @@ School_Name ENUM("School of Communication and the Arts",
 "School of Social and Behavioural Sciences", "School of Professional Programs"),
 Is_On_Committee BOOLEAN,
 Website_URL VARCHAR(150) );
-INSERT INTO Faculty VALUES (0, "Y", "Test", "User", "Test User 1", DEFAULT, DEFAULT, 0, False, "https://www.marist.edu");
+INSERT INTO Faculty VALUES (0, "Y", "Test", "User", "Test User 1", DEFAULT, DEFAULT, "School of Science", True, "https://www.marist.edu");
 
 CREATE TABLE IF NOT EXISTS Committees (Committee_ID INT PRIMARY KEY, Committee_Name TEXT);
 INSERT INTO Committees VALUES (0, "Test Committee 1");
