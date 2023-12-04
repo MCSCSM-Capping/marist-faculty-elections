2e5e5b00d30d7e6b4758739cb3e1df0fcda7085b
Hanna Sharkey <hthesharkey@gmail.com>
2023-12-04 12:05:21 -0500

    committee displays on view_and_manage, only active profiles display on admin_view

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const db = require('../database');
 const User = require('../models/userModel');
+const FacComMap = require('../models/facultyCommitteeJunction');
 
 // Admin view page (profile search) with middleware to check if user is admin
 router.get('/admin_view', async (req, res) => {
@@ -23,8 +24,16 @@ router.get('/view_and_manage/:userID', async (req, res) => {
             CWID: parseInt(req.params.userID)
         }
     });
+
+    let userCommittees;
+
+    if(reqUser[0].Is_On_Committee) {
+        userCommittees = await db.getFacultyCommittees(req.params.userID);
+    } else {
+        userCommittees = null;
+    }
     req.session.selectedUser = reqUser[0].CWID;
-    res.render('view_and_manage', {schools: User.getAttributes().School_Name.values, committees: reqCommittees, faculty: reqUser[0], selectedUser: req.session.selectedUser});
+    res.render('view_and_manage', {schools: User.getAttributes().School_Name.values, userCommittees: userCommittees, faculty: reqUser[0], selectedUser: req.session.selectedUser});
 });
 
 // Admin Query Preview
