c6a0a17bb9d29cb4e7a7f6c6d1926e17a11735e0
Annie Lee <112659408+alee0724@users.noreply.github.com>
2023-12-06 12:17:37 -0500

    Added js code to make school and committee filters work. Added 2 more test users for testing purposes.

--- misc/facultytables.sql
+++ misc/facultytables.sql
@@ -20,6 +20,8 @@ School_Name ENUM("School of Communication and the Arts",
 Is_On_Committee BOOLEAN,
 Website_URL VARCHAR(150) );
 INSERT INTO Faculty VALUES (0, "Y", "Test", "User", "Test User 1", DEFAULT, DEFAULT, "School of Science", True, "https://www.marist.edu");
+INSERT INTO Faculty VALUES (1, "Y", "Test1", "User1", "Test User 2", DEFAULT, DEFAULT, "School of Management", True, "https://www.marist.edu");
+INSERT INTO Faculty VALUES (2, "Y", "Test2", "User2", "Test User 3", DEFAULT, DEFAULT, "School of Liberal Arts", True, "https://www.marist.edu");
 
 CREATE TABLE IF NOT EXISTS Committees (Committee_ID INT PRIMARY KEY, Committee_Name TEXT);
 INSERT INTO Committees VALUES (0, "Test Committee 1");
